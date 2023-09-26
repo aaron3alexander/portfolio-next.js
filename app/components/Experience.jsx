a7ae85443f564c462cb1e9064cb1aac3f502f5ae
Aaron <aaron.3alexander@gmail.com>
2023-09-26 13:29:56 -0500

    fixed experience section

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -19,11 +19,11 @@ import Link from "next/link";
 export default function Experience() {
   return (
     <div className="p-4 mb-4 flex flex-col w-full lg:h-screen">
-      <h1 className="text-6xl lg:text-8xl text-center mt-8 mb-12 lg:mb-24">
+      <h1 className="text-6xl lg:text-8xl text-center mt-8 mb-12">
         experience
       </h1>
 
-      <div className="mb-4 flex flex-col h-full w-full space-y-4 items-center justify-evenly lg:flex-row lg:justify-center lg:items-start lg:space-x-28 lg:space-y-0">
+      <div className="flex flex-col h-full w-full space-y-4 items-center justify-evenly lg:flex-row lg:justify-center lg:items-start lg:space-x-28 lg:space-y-0">
         <div className="border-2 py-2 border-black w-full h-fit lg:w-96 lg:shadow-2xl rounded-lg">
           <div className="w-full h-full px-4 py-1 flex flex-col">
             <div className="w-full flex justify-center items-center mb-1 lg:mb-0 ">
